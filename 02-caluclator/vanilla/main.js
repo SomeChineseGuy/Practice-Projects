cbcef2868c6458b67fe8e4a0e515d4ee6b02f7f1
Alvin Ng <alvin.cl.ng@gmail.com>
2022-04-20 13:13:32 -0700

    Got items being pushed into array and displaying correctly

--- 02-caluclator/vanilla/main.js
+++ 02-caluclator/vanilla/main.js
@@ -1,6 +1,11 @@
+let space = [];
 document.addEventListener('click', (e) => {
-  
+  const calucalatorResults = document.querySelector('.calculator-screen');
   if(e.target.nodeName === "BUTTON") {
-    console.log(e.target.getAttribute('data-button-type'));
+    let buttonPress = e.target.getAttribute('data-button-type');
+    space.push(buttonPress);
+    console.log(space);
+    let display = space.join('');
+    calucalatorResults.innerHTML = display;
   }
 })
