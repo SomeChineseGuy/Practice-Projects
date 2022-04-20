bb19c01962208f5ef5b618a07bf728f46c747e7d
Alvin Ng <alvin.cl.ng@gmail.com>
2022-04-20 16:20:33 -0700

    Updated html calc function and added basic math funciton but not the equals

--- 02-caluclator/vanilla/main.js
+++ 02-caluclator/vanilla/main.js
@@ -1,20 +1,51 @@
 let space = [];
+let prevNum = 0;
+let calcType = "";
+const empty = arr => arr.length = 0;
+let calc = {
+  "+": (num1, num2) => {
+    return num1 + num2
+  },
+  "-": (num1, num2) => {
+    return num1 - num2
+  },
+  "/": (num1, num2) => {
+    return num1 / num2
+  },
+  "*": (num1, num2) => {
+    return num1 * num2
+  },
+  "=": (num1, num2) => {
+    return num1 * num2
+  },
+};
+const calucalatorResults = document.querySelector('.calculator-screen');
 document.addEventListener('click', (e) => {
-  const calucalatorResults = document.querySelector('.calculator-screen');
   let buttonPress = e.target.getAttribute('data-button-type');
   if(e.target.nodeName === "BUTTON") {
     let isCalculateButton = e.target.getAttribute('data-is-calc');
     if(!isCalculateButton) {
       space.push(buttonPress);
-      console.log(space);
       let display = space.join('');
       calucalatorResults.innerHTML = display;
     } else {
       if(buttonPress === "clear") {
         calucalatorResults.innerHTML = 0;
-        const empty = arr => arr.length = 0;
-        empty(space);
+        prevNum = 0;      
+        empty(space);        
+      }
+      if(isCalculateButton === "true") {
+        let calcResults = Number(space.join(''));
+        if(prevNum === 0) {
+          prevNum += calcResults
+          empty(space);
+          return calucalatorResults.innerHTML = 0;      
+        };
+        console.log(prevNum, buttonPress, calcResults)
+        calcType = buttonPress;
+        calucalatorResults.innerHTML = calc[buttonPress](prevNum, calcResults);      
+        // console.log(results)
       }
     }
   }
-})
+});
