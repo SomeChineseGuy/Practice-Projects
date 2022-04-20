8fdc3f00d9dcbbcbcc3f01ee974ff1cef86daeb1
Alvin Ng <alvin.cl.ng@gmail.com>
2022-04-20 13:33:21 -0700

    Added clear function

--- 02-caluclator/vanilla/main.js
+++ 02-caluclator/vanilla/main.js
@@ -1,11 +1,20 @@
 let space = [];
 document.addEventListener('click', (e) => {
   const calucalatorResults = document.querySelector('.calculator-screen');
+  let buttonPress = e.target.getAttribute('data-button-type');
   if(e.target.nodeName === "BUTTON") {
-    let buttonPress = e.target.getAttribute('data-button-type');
-    space.push(buttonPress);
-    console.log(space);
-    let display = space.join('');
-    calucalatorResults.innerHTML = display;
+    let isCalculateButton = e.target.getAttribute('data-is-calc');
+    if(!isCalculateButton) {
+      space.push(buttonPress);
+      console.log(space);
+      let display = space.join('');
+      calucalatorResults.innerHTML = display;
+    } else {
+      if(buttonPress === "clear") {
+        calucalatorResults.innerHTML = 0;
+        const empty = arr => arr.length = 0;
+        empty(space);
+      }
+    }
   }
 })
