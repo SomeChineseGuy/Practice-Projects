f98ee121491a41f9a05625786dff1f5c78ec0019
Alvin Ng <alvin.cl.ng@gmail.com>
2022-04-28 22:36:04 -0700

    Added axios and got the first API to work

--- 03-LOL-search/server.js
+++ 03-LOL-search/server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
-const port = 4200;
-const app = express()
+const env = require('dotenv').config({
+  path: __dirname + '/.env'
+})
+const port = process.env.PORT;
+const app = express();
+const axios = require('axios');
+app.use(morgan('dev'));
 
-app.use(morgan);
+
+app.get('/', (req, res) => {
+  res.send("Ready");
+})
+
+const getSummerID = async (summonerName = "ThatGuyy") => {
+  const summonerID = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.riot_api_key}`)
+    .then(data => {
+      return data.data.id;
+    });
+    return summonerID;
+}
+
+const testAPI = async () => {
+  let summonerID = await getSummerID()
+  console.log(summonerID);
+}
+
+testAPI();
 
 app.listen(port, ()=> {
   console.log(`app is listening on port ${port}`);
