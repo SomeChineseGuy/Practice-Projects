955683f11bd146672098d296fb818226d06cf3ee
Alvin Ng <alvin.cl.ng@gmail.com>
2022-05-03 15:07:41 -0700

    Created all the componets needed for project

---
+++ 04-form/form/src/componets/DropDownContainer.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const DropDownContainer = () => {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  )
+}
---
+++ 04-form/form/src/componets/FormContainer.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { DropDownContainer } from "./DropDownContainer";
+
+export const FormContainer = () => {
+  const [formData, setFormData] = useState([
+    {
+      id: 1,
+      name: 'Driver',
+      placeholder: '',
+      value: '',
+      type: 'option',
+      width: '6',
+      prop: 'driver',
+      required: true,
+      options: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
+    },
+    {
+      id: 2,
+      name: 'Truck',
+      placeholder: '',
+      value: '',
+      type: 'option',
+      width: '6',
+      prop: 'driver',
+      required: true,
+      options: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
+    },
+    {
+      id: 3,
+      name: 'Trailer',
+      placeholder: '',
+      value: '',
+      type: 'option',
+      width: '6',
+      prop: 'trailer',
+      required: true,
+      options: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
+    },
+    {
+      id: 4,
+      name: 'Temp',
+      placeholder: '',
+      value: '',
+      type: 'number',
+      width: '6',
+      prop: 'temp',
+      required: true,
+    },
+  ]);
+  return (
+    <div>
+      <h1>Form Title</h1>
+      <DropDownContainer />
+
+    </div>
+  )
+}
