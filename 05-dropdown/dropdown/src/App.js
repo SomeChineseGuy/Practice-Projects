5c872172f0572a2c27ca7d5b10880f54c55b4370
Alvin Ng <alvin.cl.ng@gmail.com>
2022-05-09 12:47:16 -0700

    Got based logic down

--- 05-dropdown/dropdown/src/App.js
+++ 05-dropdown/dropdown/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [colorArr, setColorArr] = useState(['red', 'green', 'blue', 'pink']);
+  const [currentColor, setCurrentColor] = useState('red');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const DropDownItem = (props) => {
+    return(
+      <li onClick={()=> setCurrentColor(props.children)}>
+        <p>{props.children}</p>
+      </li>
+    )
+  }
+
+
   return (
     <div className="App">
-      
+      <h1>Hello world</h1>
+      <div className='bar' style={{backgroundColor: `${currentColor}`}}></div>
+      <div className='dropdown-conatiner' onClick={() => setIsOpen(!isOpen)}>
+        <ul>
+        {colorArr.map((item) => {
+          return <DropDownItem key={item}>{item}</DropDownItem>
+        })}
+        </ul>
+      </div>
     </div>
   );
 }
