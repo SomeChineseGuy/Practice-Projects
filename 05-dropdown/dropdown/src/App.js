815341f623c59a6da4f31208ffe82a67b6f6b983
Alvin Ng <alvin.cl.ng@gmail.com>
2022-05-09 13:39:12 -0700

    Final code changes

--- 05-dropdown/dropdown/src/App.js
+++ 05-dropdown/dropdown/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [colorArr, setColorArr] = useState(['red', 'green', 'blue', 'pink']);
-  const [currentColor, setCurrentColor] = useState('red');
+  const [colorArr, setColorArr] = useState(['Red', 'Green', 'Blue', 'Pink']);
+  const [currentColor, setCurrentColor] = useState('Red');
   const [isOpen, setIsOpen] = useState(false);
 
   const DropDownItem = (props) => {
     return(
       <li className='dropdown-item' onClick={()=> setCurrentColor(props.children)}>
-        <p>{isOpen && props.children}</p>
+        <p>{props.children}</p>
       </li>
     )
   }
@@ -21,7 +21,7 @@ function App() {
       <div className='bar' style={{backgroundColor: `${currentColor}`}}></div>
       <div className='dropdown-conatiner' onClick={() => setIsOpen(!isOpen)}>
         <ul >
-          <li style={isOpen ? {fontStyle: 'italic', opacity: '0.5'} : {}}>{isOpen ? 'Select' : currentColor}</li>
+          <li style={isOpen ? {fontStyle: 'italic', opacity: '0.5'} : {} }>{isOpen ? 'Select' : currentColor}</li>
           {isOpen && colorArr.map((item, index) => {
             return <DropDownItem key={index}>{item}</DropDownItem>
           })}
