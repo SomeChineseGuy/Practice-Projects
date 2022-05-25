ba9fad4f3baefc474e89636f5d6f2538e8e2ce84
Alvin Ng <alvin.cl.ng@gmail.com>
2022-05-24 21:10:28 -0700

    Got number and input fixed

--- 04-form/form/src/App.js
+++ 04-form/form/src/App.js
@@ -1,12 +1,21 @@
 import './variable.css';
 import './App.css';
-
-import {FormContainer} from './componets/FormContainer';
 import {DropDownItem} from './componets/DropdownItem';
 import {useState} from 'react';
 
 function App() {
-  const [items, setItems] = useState([{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]);
+  const [items, setItems] = useState([
+  {
+    name: "Driver", 
+    type: 'dropdown',
+    elements: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
+  },
+  {
+    name: "Temp", 
+    type: 'number'
+  },
+
+  ]);
   const [selected, setSelected] = useState(null);
 
   const pickUser = (selection) => {
@@ -15,7 +24,16 @@ function App() {
 
   return (
     <div className="App">
-      <DropDownItem items={items} selected={selected} pickUser={pickUser}/>
+      {items && items.map((item) => {
+        return <DropDownItem 
+          elements={item.elements} 
+          name={item.name} 
+          type={item.type} 
+          selected={selected} 
+          pickUser={pickUser}
+        />
+      })}
+      
       <h1>{selected}</h1>
     </div>
   );
--- 04-form/form/src/componets/DropdownItem.js
+++ 04-form/form/src/componets/DropdownItem.js
@@ -7,21 +7,33 @@ export const DropDownItem = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return(
-    <div className={`dropdown-container ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
-      <li className='selected' style={isOpen ? {fontStyle: 'italic', opacity: '0.5'} : {} }>
-        {props.selected ? props.selected : "Select"}
-      </li>
-      <ul>
-        
-        {isOpen && props.items.map(item => {
-          return (
-          <li key={item.id} onClick={(e) => props.pickUser(item.value)}>
-            {item.value}
+    <div>
+      <h3>{props.name}</h3>
+      {props.type === "dropdown" &&
+        <div className={`dropdown-container ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+          <li className='selected' style={isOpen ? {fontStyle: 'italic', opacity: '0.5'} : {opacity: .7} }>
+            {props.selected ? props.selected : "Select"}
           </li>
-          )
-        })}
-        <FontAwesomeIcon icon={faChevronDown}  className={`icon ${isOpen ? 'up' : ''}`} />
-      </ul>
+          <ul>
+            
+            {isOpen && props.elements.map(item => {
+              return (
+              <li key={item.id} onClick={(e) => props.pickUser(item.value)}>
+                {item.value}
+              </li>
+              )
+            })}
+            <FontAwesomeIcon icon={faChevronDown}  className={`icon ${isOpen ? 'up' : ''}`} />
+          </ul>
+        </div>
+      }
+
+      {props.type === "number" &&
+        <div> 
+          <input type="number" placeholder="Number" className="number"/>
+        </div>
+      }
+
     </div>
   )
 }
