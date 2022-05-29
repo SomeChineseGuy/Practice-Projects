ca70f7a3bfc3a91759d595ece92705233b60b304
Alvin Ng <alvin.cl.ng@gmail.com>
2022-05-29 14:11:18 -0700

    Removed none use file and updated components

--- 04-form/form/src/App.js
+++ 04-form/form/src/App.js
@@ -1,14 +1,13 @@
 import './variable.css';
 import './App.css';
-import {FormItem} from './componets/FormItem';
 import {FormContainer} from './componets/FormContainer';
 import {useState} from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGripVertical } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   const [items, setItems] = useState(
+    [
     {
       id: 5,
       sectionTitle: 'Shipment 1',
@@ -65,49 +64,152 @@ function App() {
               prop: 'cases',
             }
           ]
-        }
+        },
+        {
+          id: 26,
+          type: 'element-group',
+          width: '12',
+          list: [
+            {
+              id: 27,
+              entity_id: '',
+              name: 'Customer',
+              placeholder: 'String',
+              value: '',
+              type: 'string',
+              width: '3',
+              prop: 'customer',
+            },
+            {
+              id: 28,
+              entity_id: '',
+              name: 'Purchase Order No.',
+              placeholder: 'String',
+              value: '',
+              type: 'string',
+              width: '3',
+              prop: 'purchase_order_no',
+            },
+            {
+              id: 29,
+              entity_id: '',
+              name: 'Shipper',
+              placeholder: 'String',
+              value: '',
+              type: 'string',
+              width: '3',
+              prop: 'shipper',
+            },
+            {
+              id: 210,
+              entity_id: '',
+              name: 'Cases',
+              placeholder: 'Number',
+              value: '',
+              type: 'number',
+              width: '3',
+              prop: 'cases',
+            }
+          ]
+        },
+        {
+          id: 36,
+          type: 'element-group',
+          width: '12',
+          list: [
+            {
+              id: 37,
+              entity_id: '',
+              name: 'Customer',
+              placeholder: 'String',
+              value: '',
+              type: 'string',
+              width: '3',
+              prop: 'customer',
+            },
+            {
+              id: 38,
+              entity_id: '',
+              name: 'Purchase Order No.',
+              placeholder: 'String',
+              value: '',
+              type: 'string',
+              width: '3',
+              prop: 'purchase_order_no',
+            },
+            {
+              id: 39,
+              entity_id: '',
+              name: 'Shipper',
+              placeholder: 'String',
+              value: '',
+              type: 'string',
+              width: '3',
+              prop: 'shipper',
+            },
+            {
+              id: 310,
+              entity_id: '',
+              name: 'Cases',
+              placeholder: 'Number',
+              value: '',
+              type: 'number',
+              width: '3',
+              prop: 'cases',
+            }
+          ]
+        },
       ]
-    }
-    // {
-    // sectionTitle: "Section Title",
-    // row: 2,
-    // column: 2,
-    // formType: 'normal',
-    // list: [
-    //   {
-    //     name: "Driver", 
-    //     type: 'dropdown',
-    //     id:1,
-    //     required: true,
-    //     elements: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
-    //   },
-    //   {
-    //     name: "Temp", 
-    //     type: 'number',
-    //     id: 2,
-    //     required: true,
-    //   },
+    },
+    {
+    sectionTitle: "Section Title",
+    row: 2,
+    column: 2,
+    id: 999,
+    formType: 'normal',
+    elements: [
+      {
+        id: 16,
+        type: 'element-group',
+        width: '12',
+        list: [
+          {
+            name: "Driver", 
+            type: 'dropdown',
+            id: 11,
+            required: true,
+            elements: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
+          },
+          {
+            name: "Temp", 
+            type: 'number',
+            placeholder: "Number",
+            id: 12,
+            required: true,
+          },
 
-    //   {
-    //     id: 3,
-    //     name: 'Truck',
-    //     type: 'dropdown',
-    //     width: '6',
-    //     prop: 'driver',
-    //     required: true,
-    //     elements: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
-    //   },
-    //   {
-    //     id: 4,
-    //     name: 'Trailer',
-    //     type: 'dropdown',
-    //     width: '6',
-    //     prop: 'trailer',
-    //     required: true,
-    //     elements: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
-    //   },
-    // ]}
-    );
+          {
+            id: 13,
+            name: 'Truck',
+            type: 'dropdown',
+            width: '6',
+            prop: 'driver',
+            required: true,
+            elements: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
+          },
+          {
+            id: 222,
+            name: 'Trailer',
+            type: 'dropdown',
+            width: '6',
+            prop: 'trailer',
+            required: true,
+            elements: [{ id: 1, value: 'Steve' }, { id: 2, value: 'Mike' }]
+          },
+        ]
+      }
+    ]}
+  ]);
   const [selected, setSelected] = useState(null);
 
   const pickUser = (selection) => {
@@ -117,23 +219,15 @@ function App() {
   return (
     <div className="App" >
       <h1>Form Title</h1>
-      <section className="section-container">
-        <h2>{items.sectionTitle}</h2>
-        <div className={`form-container ${items.draggable ? "draggable" : ""}` } style={{gridTemplateColumns: `repeat(${items.column}, 1fr)`}} >
-          {items.draggable && <FontAwesomeIcon icon={faGripVertical} className="drag-dots" />}
-          {items && items.elements[0].list.map((item) => {
-            return <FormItem
-              key={item.id}
-              elements={item.elements} 
-              name={item.name} 
-              type={item.type} 
-              placeholder={item.placeholder} 
-              selected={selected} 
-              pickUser={pickUser}
-            />
-          })}
-        </div>
-      </section>
+      {items && items.map(form => {
+        return <FormContainer
+          key={form.id}
+          sectionTitle={form.sectionTitle}
+          draggable={form.draggable}
+          column={form.column}
+          elements={form.elements}
+        />
+      })}
     </div>
   );
 }
--- 04-form/form/src/componets/FormContainer.js
+++ 04-form/form/src/componets/FormContainer.js
@@ -3,13 +3,28 @@ import {FormItem} from './FormItem';
 import '../variable.css';
 import './FormContainer.css'
 
-export const FormContainer = (props) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGripVertical } from '@fortawesome/free-solid-svg-icons'
 
+export const FormContainer = (props) => {
+  const {sectionTitle, draggable, column, elements} = props;
 
   
   return (
-    <div>
-      <h1>Form Title</h1>
+    <section className="section-container">
+    <h2>{sectionTitle}</h2>
+    <div className={`form-container ${draggable ? "draggable" : ""}` } style={{gridTemplateColumns: `repeat(${column}, 1fr)`}} >
+      {draggable && <FontAwesomeIcon icon={faGripVertical} className="drag-dots" />}
+      {elements && elements[0].list.map((item) => {
+        return <FormItem
+          key={item.id}
+          elements={item.elements} 
+          name={item.name} 
+          type={item.type} 
+          placeholder={item.placeholder} 
+        />
+      })}
     </div>
+  </section>
   )
 }
